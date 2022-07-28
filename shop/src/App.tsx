67a523f9810f0eaab799637cdd25b79a9e3d4671
MIN GYEONG KWON <alsruddl0805@naver.com>
2022-07-28 15:23:47 +0900

    Styled-components

--- shop/src/App.tsx
+++ shop/src/App.tsx
@@ -1,13 +1,52 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './css/reset.css';
+import './style/common.scss';
+import styled from 'styled-components';
 
-function App() {
+let BgButton = styled.button`
+background-color: black;
+`
+
+function App() :JSX.Element {
   return (
     <div className="App">
-      <p> 개인 프로젝트 - shop </p>
+      <div>
+      <Login />
+      <Button />
+      </div>
     </div>
   );
+}
+
+function Login() :JSX.Element {
+  return (
+    <div className="input-wrap">
+      <div className="tit">
+        <p className="font-500">로그인</p>
+      </div>
+      <div className="list">
+        <ul className="name">
+          <li>ID</li>
+          <li>PW</li>
+        </ul>
+        <ul className="input-t">
+          <li><input type="text" /></li>
+          <li><input type="text" /></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+function Button() :JSX.Element {
+  return (
+    <div className="btn-db">
+      <BgButton>로그인</BgButton>
+      <button>회원가입</button>
+    </div>
+  )
 }
 
 export default App;
