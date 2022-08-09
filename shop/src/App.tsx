48966e2e0ad9fe9f27a12a3451b6c010c972b74b
MIN GYEONG KWON <alsruddl0805@naver.com>
2022-08-09 13:43:32 +0900

    Component 타입 지정 테스트

--- shop/src/App.tsx
+++ shop/src/App.tsx
@@ -1,52 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import './css/reset.css';
 import './style/common.scss';
 import styled from 'styled-components';
+import { useState } from 'react';
 
 let BgButton = styled.button`
 background-color: black;
 `
 
+/*
+React + typescript 사용 시 주의 점
+
+1. 일반변수, 함수 만들때 타입지정 신경쓸 것
+2. JSX 타입 지정
+3. Component 타입 지정 (함수 타입지정 : 파라미터 / return 타입)
+*/
+
+let hi :string = '안녕하세요!';
+let box :JSX.Element = <div></div>; // JSX 표현하는 타입
+
 function App() :JSX.Element {
+  let [name, setName] = useState('');
+  
   return (
     <div className="App">
       <div>
-      <Login />
-      <Button />
+      <div>이름 : <input type='text' onChange={(e) => {setName(e.target.value)}}></input></div> 
+      <Profile name={name} age="20" />
       </div>
     </div>
   );
 }
 
-function Login() :JSX.Element {
-  return (
-    <div className="input-wrap">
-      <div className="tit">
-        <p className="font-500">로그인</p>
-      </div>
-      <div className="list">
-        <ul className="name">
-          <li>ID</li>
-          <li>PW</li>
-        </ul>
-        <ul className="input-t">
-          <li><input type="text" /></li>
-          <li><input type="text" /></li>
-        </ul>
-      </div>
-    </div>
-  )
-}
-
-function Button() :JSX.Element {
-  return (
-    <div className="btn-db">
-      <BgButton>로그인</BgButton>
-      <button>회원가입</button>
-    </div>
-  )
+function Profile(props: {name :string, age :string}) :JSX.Element {
+  return <div>{props.name}님, 안녕하세요!</div>
 }
 
 export default App;
